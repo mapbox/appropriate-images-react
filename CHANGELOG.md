5dca86c01302929b222b08c60db1d5282d13bea8
Katy DeCorah <decorah@mapbox.com>
2020-12-21 16:31:20 -0500

    Update dependencies, use React 16 (#7)
    * Update dependencies
    
    * Clean up deps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## Main
+
+- Update to React v16 and update all other dependencies.
+
 ## 1.0.0
 
 - Start this log.
--- test/scope-appropriate-image.test.js
+++ test/scope-appropriate-image.test.js
@@ -5,7 +5,7 @@ import { scopeAppropriateImage } from '../src';
 import getAppropriateImageUrl from '@mapbox/appropriate-images-get-url';
 import Enzyme, { mount } from 'enzyme';
 import { mountToJson } from 'enzyme-to-json';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
